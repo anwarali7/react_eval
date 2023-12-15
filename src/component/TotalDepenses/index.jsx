7e78727a0897710b894ba68566f1df19ae8de35d
Ali <anwar.ali75751@yahoo.fr>
2023-12-15 14:30:58 +0100

    total color transition on change

--- src/component/TotalDepenses/index.jsx
+++ src/component/TotalDepenses/index.jsx
@@ -1,13 +1,25 @@
+import { useState, useEffect } from "react";
+import './TotalDepenses.css';
+
 const TotalDepenses = ({ expenses }) => {
+    const [changed, setChanged] = useState(false);
+    
     const totalAmount = expenses.reduce((acc, expense) => acc + expense.amount, 0);
     const formatToEuros = new Intl.NumberFormat('fr-FR', {
       style: 'currency',
       currency: 'EUR',
     });
 
+    useEffect(()=> {
+        setChanged(true);
+        setTimeout(() => {
+            setChanged(false);
+        }, 10);
+    }, [expenses]);
+    
     return (
         <div>
-            <p><strong>Total : {formatToEuros.format(totalAmount)}</strong></p>
+            <p className={'total-amount ' + (changed ? 'color-changed' : '')}>Total : {formatToEuros.format(totalAmount)}</p>
         </div>
     );
 };
